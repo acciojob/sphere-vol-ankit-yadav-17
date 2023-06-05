d2ae2fb0fbad8f48b07181fe4e5b31e218e8e77d
ankit-yadav-17 <119732393+ankit-yadav-17@users.noreply.github.com>
2023-06-05 12:29:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function volume_sphere() {
+function volume_sphere(event) {
     //Write your code here
+	event.preventDefault();
+	const radius = document.getElementById("radius").value;
+	const volume -  (4/3 * Math. PI * radius ** 3);
+	document.getElementById("volume").value = volume.toFixed(4);
   
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload = function(){
+	document.getElementById('MyForm').addEventListener("submit",volume_sphere)
+}
+	
